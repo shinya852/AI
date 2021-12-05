cd7f04a92db55578186499fc43cd1b1b407fad69
shinya852 <shinya.june@gmail.com>
2021-12-05 11:33:54 +0000

    AI

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -5,15 +5,13 @@ class TodolistsController < ApplicationController
 
   def create
     list = List.new(list_params)
-     list.score = Language.get_data(list_params[:body])  #この行を追加
-    
+    list.score = Language.get_data(list_params[:body])  #この行を追加
     list.save
-    tags = Vision.get_image_data(list.image)    
+    tags = Vision.get_image_data(list.image)
     tags.each do |tag|
       list.tags.create(name: tag)
     end
     redirect_to todolist_path(list.id)
-  
   end
 
   def index
